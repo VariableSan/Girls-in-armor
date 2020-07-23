dc0ce5903a1247a468c68236b525335e44484cab
nurs-dev <nursultan_nurgaliyev@protonmail.com>
2020-07-23 20:01:22 +0600

    добавил логику transparentizeHeader добавил parallax.js в index.vue тестирую самописный parallax

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,31 @@
 <template lang="pug">
+	div
+		div.home__hero
+			v-img(
+				src='https://sun9-30.userapi.com/c858328/v858328957/1d3b46/KsvD4IOFKU4.jpg'
+			)
 
+		v-container
+			div.some-class
+				h1 huemue
 </template>
 
 <script>
 export default {
 	asyncData: async ({$axios}) => ({
-	})
+	}),
+
+	mounted() {
+	}	
 }
 </script>
 
 <style lang="sass" scoped>
+.home
+	&__hero
+		min-height: 120vh
+
+.some-class
+	min-height: 200vh
+	color: #fff
 </style>
