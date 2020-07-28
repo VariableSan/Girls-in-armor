a8e1f51377e0db2a3bfdac838984f2e0322da3ea
nurs-dev <nursultan_nurgaliyev@protonmail.com>
2020-07-28 19:42:58 +0600

    after removed aos: added adaptive to section confrontation (done)

--- pages/index.vue
+++ pages/index.vue
@@ -9,17 +9,10 @@
 import AppParallax from '~/components/home/parallax'
 import AppConfrontation from '~/components/home/confrontation'
 
-import '~/node_modules/aos/dist/aos.css'
-const AOS = require('~/node_modules/aos/dist/aos.js')
-
 export default {
 	components: {
 		AppParallax,
 		AppConfrontation
-	},
-
-	mounted() {
-		AOS.init()
 	}
 }
 </script>
