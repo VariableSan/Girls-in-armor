1b46326c2c49d9d69834cfc76159b60495571d3e
nurs-dev <nursultan_nurgaliyev@protonmail.com>
2020-07-31 20:03:01 +0600

    footer.vue: added a link to the icon

--- components/footer.vue
+++ components/footer.vue
@@ -1,3 +1,4 @@
 <template lang="pug">
-	v-footer(dark padless absolute)
+	v-footer(dark padless)
+		//- Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 </template>
