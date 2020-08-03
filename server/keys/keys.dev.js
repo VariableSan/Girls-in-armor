3b64a976cb662f4d073b46ad4cef8e9b51bf5759
nurs-dev <nursultan_nurgaliyev@protonmail.com>
2020-08-03 12:14:08 +0600

    backend: added JWT parameter to keys.prod and keys.dev

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://JudgementHC:4KZkQLq&C$m@webfolio-qilbf.mongodb.net/girls-in-armor?retryWrites=true&w=majority'
+	mongoURI: 'mongodb+srv://JudgementHC:4KZkQLq&C$m@webfolio-qilbf.mongodb.net/girls-in-armor?retryWrites=true&w=majority',
+	JWT: 'dev-jwt-key'
 }
