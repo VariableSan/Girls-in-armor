ea149de9cd04931a7006716fa1eec0fafe674fc0
nurs-dev <nursultan_nurgaliyev@protonmail.com>
2020-09-06 15:59:04 +0600

    added sweetalert2

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,8 @@ export default {
 	*/
 	modules: [
 		// Doc: https://axios.nuxtjs.org/usage
-		'@nuxtjs/axios'
+		'@nuxtjs/axios',
+		'vue-sweetalert2/nuxt'
 	],
 	/*
 	** Axios module configuration
